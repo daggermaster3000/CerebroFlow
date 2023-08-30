6e192b1b499484c96b423b8322c76e991e06cae6
Daggermaster3000 <quillan.favey@gmail.com>
2023-08-30 13:14:08 +0200

    .l..

--- Flowj.py
+++ Flowj.py
@@ -6,7 +6,7 @@ import PySimpleGUI as sg
 path = sg.popup_get_file("", no_window=True, default_extension=".tif")
 print("Image path: ",path.replace("/","\\"))
 # pixel size: 0.16250000000000003 or 0.189 (pub)
-exp1 = ky.Kymo(path.replace("/","\\"), pixel_size=0.16250000000000003, frame_time=0.291)
+exp1 = ky.Kymo(path.replace("/","\\"), pixel_size=0.189, frame_time=0.1)
 
 #exp1.test_filter()
 #exp1.test_threshold()
