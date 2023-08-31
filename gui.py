cb644b986462043711a2eeadc88037818c941833
Daggermaster3000 <quillan.favey@gmail.com>
2023-08-31 17:40:30 +0200

    ...

--- gui.py
+++ gui.py
@@ -153,6 +153,14 @@ class GUI:
                     del exp
                     self.window["progressbar"].update((ind+1)/len(paths)*100)
 
+                if csv_table:
+                    # save data as csv
+                    print("Saving csv")
+                    df = pd.DataFrame(data=output)
+                    print(df)
+                    csv_filename = f"{output_folder}\\{group_name}_csf_flow_results.csv"
+                    df.to_csv(csv_filename, index=False)
+
                 if total_profile:
 
                     # plot total profile (mean of means)
@@ -189,13 +197,33 @@ class GUI:
                     
                     plt.close(fig)    # close the figure window
 
-                if csv_table:
-                    # save data as csv
-                    print("Saving csv")
-                    df = pd.DataFrame(data=output)
-                    print(df)
-                    csv_filename = f"{output_folder}\\{group_name}_csf_flow_results.csv"
-                    df.to_csv(csv_filename, index=False)
+                    # scatter plot
+                    """
+                    fig2, ax2 = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
+                    plt.style.use('Solarize_Light2')
+                    ax2.set_title(group_name+" CSF profile")
+                    ax2.set_xlabel(r"Absolute Dorso-ventral position [$\mu$m]")
+                    ax2.set_ylabel(r"Average rostro-caudal velocity [$\mu$m/s]")
+                    
+                    y = df["means"].values.tolist()
+                    print(y)
+                    # make all the arrays start at the same location
+                    for ind,array in enumerate(y):
+                        y[ind] = array[np.nonzero(array)[0]]
+                    # Pad each array to match the maximum length
+                    y = [np.pad(arr, (5, max_length - len(arr)+5), mode='constant', constant_values=0) for arr in y]
+                    x =  np.arange(-(len(y)-(len(y)-np.nonzero(y)[0][0])),len(y)-np.nonzero(y)[0][0])*pixel_size # find start of canal based on first non zero speed
+                    ax2.scatter(x,y,df["name"].values.tolist()) 
+                    ax2.legend()
+
+                    if output_folder:
+                        fig2.savefig(output_folder+"\\"+group_name+"_scatter_total_vel_threshold"+str(np.round(threshold,1))+"_filter"+str(filter_size)+'.png')   # save the figure to file
+                    else:
+                        fig2.savefig(group_name+"_scatter_total_vel_threshold"+str(np.round(threshold,1))+"_filter"+str(filter_size)+'.png')   # save the figure to file
+                    
+                    plt.close(fig2)    # close the figure window
+                    """
+
 
                 subprocess.Popen(f'explorer "{output_folder}"')
                 self.analysis_running = False
